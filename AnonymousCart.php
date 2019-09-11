53c428b505205290de40f1d553729ad7dacd33ec
GamitJoseph <gamit.joseph.73@gmail.com>
2019-09-11 13:31:15 +0530

    cart

--- AnonymousCart.php
+++ AnonymousCart.php
@@ -8,14 +8,19 @@ if (isset($_GET["cid"])) {
 	{
 		header("location:index.php");
 	}else{
-	$visitor_id=$_COOKIE["visitor"];
-	$user=$_COOKIE["visitor"];
-	$qr="select * from cart where pid=$cid";
-	$rs=db->QueryTable($qr);
-	 if (mysqli_num_rows($rs)==0) {
-
-        }
-	$query="INSERT INTO cart(cid, visitor_id, pid) VALUES (null,'$visitor_id','$id')";
+		$visitor_id=$_COOKIE["visitor"];
+		$user=$_COOKIE["visitor"];
+		$qr="select * from cart where pid=$cid";
+		$rs=db->QueryTable($qr);
+		if (mysqli_num_rows($rs)==0) {
+			$query="INSERT INTO cart(cid, visitor_id, pid) VALUES (null,'$visitor_id','$id')";
+			db->QueryTable($query);
+			header("location:CartList.php?msg=success");
+		}
+		else{
+			header("location:CartList.php?msg=already");
+		}
+		
 
 	}
 
